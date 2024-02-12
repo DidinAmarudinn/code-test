0b28b3049029fbbf2511571b5fba6d3f5d4bb8ed
Didin Amarudin <amardidin53@gmail.com>
2024-02-12 09:02:19 +0700

    first commit

---
+++ Coding-Test/App/Coding_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coding_TestApp.swift
+//  Coding-Test
+//
+//  Created by didin amarudin on 11/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Coding_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
